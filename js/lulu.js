276d0d1d84ff08613b654ed03bfa156078734a7c
wanglu <736990398@qq.com>
2017-07-06 10:19:33 +0800

    233

---
+++ js/lulu.js
@@ -0,0 +1,52 @@
+$('.nvshi').mouseover(
+	function(){
+		$('.shiyan').css("display","block");
+		$('.nvshi').css("color","#fff");
+		
+	}
+);
+$('.nvshi').mouseout(
+	function(){
+		$('.shiyan').css("display","none");
+		
+	}
+);
+$('.shiyan').mouseover(
+	function(){
+		$('.shiyan').css("display","block");
+		
+	}
+);
+$('.shiyan').mouseout(
+	function(){
+		$('.shiyan').css("display","none");
+		
+	}
+);
+
+
+$('.nanshi').mouseover(
+	function(){
+		$('.shiyan01').css("display","block");
+		$('.nanshi').css("color","#fff");
+		
+	}
+);
+$('.nanshi').mouseout(
+	function(){
+		$('.shiyan01').css("display","none");
+		
+	}
+);
+$('.shiyan01').mouseover(
+	function(){
+		$('.shiyan01').css("display","block");
+		
+	}
+);
+$('.shiyan01').mouseout(
+	function(){
+		$('.shiyan01').css("display","none");
+		
+	}
+);
